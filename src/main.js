ce5ca46143f830023d5e30a200285b3206b858c3
Deiko138 <leonardolopezsegundob@gmail.com>
2026-04-27 17:21:20 -0500

    feat(router): add vue-router integration and update dependencies

--- src/main.js
+++ src/main.js
@@ -18,11 +18,13 @@ import {
     Toolbar
 } from "primevue";
 import i18n from "./i18n.js";
+import router from "./router.js";
 
 
 
 createApp(App)
     .use(i18n)
+    .use(router)
     .use(Primevue, {theme: {preset: Material}, ripple: true})
     .use(ConfirmationService)
     .use(dialogService)
